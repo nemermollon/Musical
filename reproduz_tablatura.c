4f9a07e7e11eb1f9ae6864ba44ed24830d4a6a22
Nemer <nemeralbano@hotmail.com>
2025-06-23 15:15:08 -0300

    Create reproduz_tablatura.c

---
+++ reproduz_tablatura.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <windows.h>
+#include <time.h>
+
+#define delay(a) Sleep(a)
+#define beep(a,b) Beep(a,b)
+#define SPEAKER 11
+#define DURACAO 100
+#define INTERVALO 100
+
+const int NE0  = 330;
+const int NF0  = 349;
+const int NFs0 = 370;
+const int NG0  = 392;
+const int NGs0 = 415;
+const int NA0  = 440;
+const int NAs0 = 466;
+const int NB0  = 494;
+const int NC0  = 523;
+const int NCs0 = 554;
+const int ND0  = 587;
+const int NDs0 = 622;
+
+const int NE1  = 659;
+const int NF1  = 698;
+const int NFs1 = 740;
+const int NG1  = 784;
+const int NGs1 = 831;
+const int NA1  = 880;
+const int NAs1 = 932;
+const int NB1  = 988;
+const int NC1  = 1046;
+const int NCs1 = 1108;
+const int ND1  = 1175;
+const int NDs1 = 1244;
+
+const int NE2  = 1318;
+const int NF2  = 1397;
+const int NFs2 = 1480;
+const int NG2  = 1568;
+const int NGs2 = 1664;
+const int NA2  = 1760;
+const int NAs2 = 1866;
+const int NB2  = 1973;
+const int NC2  = 2096;
+const int NCs2 = 2217;
+const int ND2  = 2349;
+const int NDs2 = 2489;
+
+const int NE3  = 2637;
+const int NF3  = 2794;
+const int NFs3 = 2960;
+const int NG3  = 3136;
+const int NGs3 = 3322;
+const int NA3  = 3520;
+const int NAs3 = 3729;
+const int NB3  = 3951;
+const int NC3  = 3951;
+const int NCs3 = 3951;
+const int ND3  = 3951;
+const int NDs3 = 3951;
+
+int main(int argc, char *argv)
+{
+/* Inserir texto gerado pelo executável "le_tablatura.exe" */
+return 0;
+}
